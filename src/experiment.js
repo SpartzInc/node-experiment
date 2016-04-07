cd0157c0c64f6624ef2eda1e780ad681b7e2d85e
Matt Stypa <mstypa@dose.com>
2016-04-07 13:03:34 -0500

    Updated experiment module to use the new feature module

--- src/experiment.js
+++ src/experiment.js
@@ -1,15 +1,95 @@
 module.exports = experiment;
 
-var core = require('./core');
+var feature = require('@dose/feature');
 
 /**
- * Returns configured core object
+ * Get a configured experiments object that exposes getDigest function
  *
- * @param {object} config
+ * @param {object} experiments
  * @returns {object}
  */
-function experiment(config) {
-    core.setExperiments(config);
+function experiment(experiments) {
+    return {
+        experiments: feature(_getParsedExperimentConfig(experiments)),
+        getDigest: function(context) {
+            return _getDigest(this.experiments, context);
+        }
+    };
+}
 
-    return core;
+/**
+ * Gets experiment digest
+ *
+ * @param {object} experiments
+ * @param {string} context
+ * @returns {Object}
+ * @private
+ */
+function _getDigest(experiments, context) {
+    var experimentDigest = {};
+    var digest = experiments.getDigest(context);
+
+    experimentDigest[digest.experiments] = digest['experiment.' + digest.experiments];
+
+    return experimentDigest;
+}
+
+/**
+ * Gets parsed experiments configuration
+ *
+ * @param {object} experiments
+ * @returns {object}
+ * @private
+ */
+function _getParsedExperimentConfig(experiments) {
+    var experiment;
+    var experimentWeight;
+    var totalWeight = 0;
+    var parsedExperiments = {
+        experiments: {}
+    };
+
+    if (typeof experiments !== 'object') {
+        throw Error('Invalid configuration');
+    }
+
+    // Parse important data from the experiment configuration
+    for (experiment in experiments) {
+        experimentWeight = _getSanitizedWeight(experiments[experiment].weight);
+
+        // Ignore 0 weight experiments
+        if (experimentWeight === 0) {
+            continue;
+        }
+
+        totalWeight += experimentWeight;
+        parsedExperiments.experiments[experiment] = experimentWeight;
+        parsedExperiments['experiment.' + experiment] = experiments[experiment].variants;
+    }
+
+    // Convert weights into odds
+    for (experiment in parsedExperiments.experiments) {
+        parsedExperiments.experiments[experiment] = parsedExperiments.experiments[experiment] / totalWeight * 100;
+    }
+
+    // Make last experiment 100 to ensure complete coverage
+    if (Object.keys(parsedExperiments.experiments).length) {
+        parsedExperiments.experiments[experiment] = 100;
+    }
+
+    return parsedExperiments;
+}
+
+/**
+ * Gets a normalized weights
+ *
+ * @param {*} weight
+ * @returns {number}
+ * @private
+ */
+function _getSanitizedWeight(weight) {
+    if (isNaN(weight)) {
+        return 0;
+    }
+    return weight < 0 ? 0 : weight;
 }
--- tests/experiment-test.js
+++ tests/experiment-test.js
@@ -1,22 +1,112 @@
 jest.dontMock('../src/experiment');
+jest.mock('@dose/feature');
 
 describe('experiment', function() {
 
     var experiment = require('../src/experiment');
-    var core = require('../src/core');
+    var feature = require('@dose/feature');
 
-    it('should export constructor', function() {
-        expect(typeof experiment).toEqual('function');
+    var _experimentConfig = {
+        experimentA: {
+            weight: 10,
+            variants: 100,
+            slug: '123'
+        },
+        experimentB: {
+            weight: 10,
+            variants: [
+                'variantA',
+                'variantB',
+                'variantC'
+            ],
+            slug: '234'
+        },
+        experimentC: {
+            weight: 20,
+            variants: {
+                variantA: 10,
+                variantB: 10,
+                variantC: 20
+            },
+            slug: '345'
+        },
+        experimentD: {
+            weight: 0,
+            variants: [
+                'variantA'
+            ],
+            slug: '456'
+        },
+        experimentE: {
+            weight: -100,
+            variants: [
+                'variantA'
+            ],
+            slug: '567'
+        },
+        experimentF: {
+            weight: 'NaN',
+            variants: [
+                'variantA'
+            ],
+            slug: '789'
+        }
+    };
+
+    beforeEach(function() {
+        feature.mockClear();
     });
 
-    it('should set experiment configuration', function() {
-        var configuration = {'experiment': 100};
+    it('should export instantiated experiment object', function() {
+        var experiments = experiment({});
 
-        experiment(configuration);
-        expect(core.setExperiments.mock.calls[0][0]).toEqual(configuration);
+        expect(typeof experiments).toEqual('object');
+        expect(typeof experiments.getDigest).toEqual('function');
     });
 
-    it('should return core', function() {
-        expect(experiment({})).toEqual(core);
+    it('throws an error when configuration is invalid', function() {
+        expect(function() {
+            experiment('Not and Object');
+        }).toThrow();
+    });
+
+    it('sets feature configuration', function() {
+        experiment(_experimentConfig);
+
+        expect(feature.mock.calls[0][0]).toEqual({
+            experiments: {
+                experimentA: 25,
+                experimentB: 25,
+                experimentC: 100
+            },
+            'experiment.experimentA': 100,
+            'experiment.experimentB': [
+                'variantA',
+                'variantB',
+                'variantC'
+            ],
+            'experiment.experimentC': {
+                variantA: 10,
+                variantB: 10,
+                variantC: 20
+            },
+        });
+    });
+
+    it('gets digest', function() {
+        feature.mockReturnValue({
+            getDigest: function() {
+                return {
+                    experiments: 'experimentA',
+                    'experiment.experimentA': 'variantA'
+                };
+            }
+        });
+
+        var experiments = experiment(_experimentConfig);
+
+        expect(experiments.getDigest('context')).toEqual({
+            experimentA: 'variantA'
+        });
     });
 });
