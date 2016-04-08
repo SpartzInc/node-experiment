5bc9d92a3bfc1fb48a72421f980322aeee4a8e51
Matt Stypa <mstypa@dose.com>
2016-04-08 14:41:22 -0500

    Added slug overrides
    Booleans can now be used to define odds
    
    Rollback of the last commit.
    
    Restructured the module to hide private attributes

--- src/experiment.js
+++ src/experiment.js
@@ -9,90 +9,196 @@ var feature = require('@dose/feature');
  * @returns {object}
  */
 function experiment(experiments) {
-    return {
-        experiments: feature(_getParsedExperimentConfig(experiments)),
-        getDigest: function(context) {
-            return _getDigest(this.experiments, context);
-        }
-    };
-}
-
-/**
- * Gets experiment digest
- *
- * @param {object} experiments
- * @param {string} context
- * @returns {Object}
- * @private
- */
-function _getDigest(experiments, context) {
-    var experimentDigest = {};
-    var digest = experiments.getDigest(context);
-
-    experimentDigest[digest.experiments] = digest['experiment.' + digest.experiments];
-
-    return experimentDigest;
-}
-
-/**
- * Gets parsed experiments configuration
- *
- * @param {object} experiments
- * @returns {object}
- * @private
- */
-function _getParsedExperimentConfig(experiments) {
-    var experiment;
-    var experimentWeight;
-    var totalWeight = 0;
-    var parsedExperiments = {
-        experiments: {}
-    };
-
     if (typeof experiments !== 'object') {
         throw Error('Invalid configuration');
     }
 
-    // Parse important data from the experiment configuration
-    for (experiment in experiments) {
-        experimentWeight = _getSanitizedWeight(experiments[experiment].weight);
+    /**
+     * Parsed slug map. This helps us to generate override objects.
+     *
+     * @type {Object}
+     * @private
+     */
+    var _slugMap = _getParsedSlugMap(experiments);
 
-        // Ignore 0 weight experiments
-        if (experimentWeight === 0) {
-            continue;
+    /**
+     * Feature object configured for experiments.
+     *
+     * @type {Object}
+     * @private
+     */
+    var _experiments = feature(_getParsedExperimentConfig(experiments));
+
+    /**
+     * Gets experiment digest
+     *
+     * @param {string} context
+     * @param {object|string} override
+     * @returns {Object}
+     */
+    function getDigest(context, override) {
+        var experimentDigest = {};
+
+        var digest = _experiments.getDigest(context, _getParsedOverride(override));
+
+        experimentDigest[digest.experiments] = digest['experiment.' + digest.experiments];
+
+        return experimentDigest;
+    }
+
+    /**
+     * Gets parsed experiments configuration
+     *
+     * @param {object} experiments
+     * @returns {object}
+     * @private
+     */
+    function _getParsedExperimentConfig(experiments) {
+        var experiment;
+        var experimentWeight;
+        var totalWeight = 0;
+        var parsedExperiments = {
+            experiments: {}
+        };
+
+        // Parse important data from the experiment configuration
+        for (experiment in experiments) {
+            experimentWeight = _getSanitizedWeight(experiments[experiment].weight);
+
+            // Ignore 0 weight experiments
+            if (experimentWeight === 0) {
+                continue;
+            }
+
+            totalWeight += experimentWeight;
+            parsedExperiments.experiments[experiment] = experimentWeight;
+            parsedExperiments['experiment.' + experiment] = experiments[experiment].variants;
         }
 
-        totalWeight += experimentWeight;
-        parsedExperiments.experiments[experiment] = experimentWeight;
-        parsedExperiments['experiment.' + experiment] = experiments[experiment].variants;
+        // Convert weights into odds
+        for (experiment in parsedExperiments.experiments) {
+            parsedExperiments.experiments[experiment] = parsedExperiments.experiments[experiment] / totalWeight * 100;
+        }
+
+        // Make last experiment 100 to ensure complete coverage
+        if (Object.keys(parsedExperiments.experiments).length) {
+            parsedExperiments.experiments[experiment] = 100;
+        }
+
+        return parsedExperiments;
     }
 
-    // Convert weights into odds
-    for (experiment in parsedExperiments.experiments) {
-        parsedExperiments.experiments[experiment] = parsedExperiments.experiments[experiment] / totalWeight * 100;
+    /**
+     * Gets parsed slug map
+     *
+     * @param {object} experiments
+     * @returns {object}
+     * @private
+     */
+    function _getParsedSlugMap(experiments) {
+        var slugMap = {};
+        var variants;
+
+        // Parse important data from the experiment configuration
+        for (var experiment in experiments) {
+            variants = experiments[experiment].variants;
+
+            if (typeof variants === 'object') {
+                if (!Array.isArray(variants)) {
+                    variants = Object.keys(variants);
+                }
+            } else {
+                variants = [];
+            }
+
+            slugMap[experiments[experiment].slug] = {
+                name: experiment,
+                variants: variants
+            };
+        }
+
+        return slugMap;
     }
 
-    // Make last experiment 100 to ensure complete coverage
-    if (Object.keys(parsedExperiments.experiments).length) {
-        parsedExperiments.experiments[experiment] = 100;
+    /**
+     * Gets parsed overrides from slug map
+     *
+     * @param {object|string} override
+     * @returns {object}
+     * @private
+     */
+    function _getParsedOverride(override) {
+        var parsedOverride = {};
+
+        if (!override) {
+            return {};
+        }
+
+        try {
+            parsedOverride = JSON.parse(override);
+        } catch (e) {
+            parsedOverride = override;
+        }
+
+        if (typeof parsedOverride !== 'object') {
+            var overrideParts = override.toString().split('-');
+            parsedOverride = {};
+            parsedOverride[overrideParts[0]] = overrideParts[1];
+        }
+
+        var slug;
+        var variant;
+
+        if (Array.isArray(parsedOverride)) {
+            slug = parsedOverride[0];
+        } else {
+            slug = Object.keys(parsedOverride)[0];
+            variant = parsedOverride[slug];
+        }
+
+        // Turn off all experiments if slug is not found
+        if (!_slugMap.hasOwnProperty(slug)) {
+            return {
+                experiments: null
+            };
+        }
+
+        var translatedOverride = {
+            experiments: _slugMap[slug].name
+        };
+
+        if (
+            variant !== null &&
+            variant !== 'undefined' &&
+            _slugMap[slug].variants.length
+        ) {
+            variant = Math.min(_slugMap[slug].variants.length - 1, variant);
+            variant = Math.max(0, variant);
+
+            translatedOverride['experiment.' + _slugMap[slug].name] = _slugMap[slug].variants[variant];
+        }
+
+        return translatedOverride;
     }
 
-    return parsedExperiments;
-}
+    /**
+     * Gets a normalized weights
+     *
+     * @param {*} weight
+     * @returns {number}
+     * @private
+     */
+    function _getSanitizedWeight(weight) {
+        if (typeof weight === 'boolean') {
+            weight = weight ? 1 : 0;
+        } else if (typeof weight !== 'number' || isNaN(weight)) {
+            return 0;
+        }
 
-/**
- * Gets a normalized weights
- *
- * @param {*} weight
- * @returns {number}
- * @private
- */
-function _getSanitizedWeight(weight) {
-    if (typeof weight === 'boolean') {
-        weight = weight ? 1 : 0;
-    } else if (typeof weight !== 'number' || isNaN(weight)) {
-        return 0;
+        return weight < 0 ? 0 : weight;
     }
 
-    return weight < 0 ? 0 : weight;
+    return {
+        getDigest: getDigest
+    };
 }
